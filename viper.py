132473709733fb027a8d8627bb701dae15fe7937
Benjamin <schmbe@gmail.com>
2022-03-05 16:51:19 +0100

    Added support for multiple plus some refactoring.

--- viper.py
+++ viper.py
@@ -21,6 +21,7 @@ Remake of the classic game snake.
 # ****************************************************************************
 
 import configparser
+import os
 import pygame
 import pygcurse
 import random
@@ -34,40 +35,90 @@ DOWN = (0, 1)
 LEFT = (-1, 0)
 RIGHT = (1, 0)
 
-LEVEL = 'level1.ini'
-
 
 def absolute_coordinate(coordinate):
+    """Takes a coordinate on the level and returns the corresponding
+    coordinate on the window."""
     return coordinate[0], coordinate[1] + 1
 
 
 class Game:
     def __init__(self, clock):
         self.clock = clock
-        self.level = Level(LEVEL)
-        self.player = Player(LEVEL)
 
-        # Has the game been paused via the space bar.
+        config = configparser.ConfigParser()
+        config.read('settings.ini')
+
+        self.player_color = pygame.Color(config['Colors']['Player'])
+        self.boundary_color = pygame.Color(config['Colors']['Boundary'])
+        self.background_color = pygame.Color(config['Colors']['Background'])
+        self.text_color = pygame.Color(config['Colors']['Text'])
+        self.box_color = pygame.Color(config['Colors']['Box'])
+
+        self.width = int(config['Campaign']['Width'])
+        self.height = int(config['Campaign']['Height'])
+        self.initial_lives = int(config['Campaign']['Lives'])
+        self.levelmask = config['Campaign']['Levelmask']
+        self.current_level = 0
+
+        self.number_pos = (0, 0)
+        self.current_number = 0
+
+        # Will later contain the instance of the current level.
+        self.level = None
+        # Will later contain the instance of the player.
+        self.player = None
+
+        # Has the game been paused via the space bar?
         self.paused = False
-
-        # False means that the game is not running for any reason
-        self.running = True
-
-        # True if the game is beaten.
+        # Has the player won the game?
         self.won = False
+        # False means that the game is not running for any reason including
+        # pause, game over, or won.
+        self.running = True
 
         # This will be the set to the coordinates of a box on the screen later.
         self.box_region = None
 
-        self.win = pygcurse.PygcurseWindow(*self.total_dimensions(), 'Viper')
+        self.win = pygcurse.PygcurseWindow(*self.window_dimensions(), 'Viper')
+        self.next_level()
+
+    def next_level(self):
+        self.current_level += 1
+        filename = self.levelmask.replace('*', str(self.current_level))
+        if not os.path.exists(filename):
+            self.won = True
+            self.draw_box('You won! Play again? y/n')
+            return
+        self.level = Level(filename, self.width, self.height)
+        if self.player is None:
+            self.player = Player(self.level, self.initial_lives)
+        else:
+            self.player = Player(self.level, self.player.lives)
+
+        self.current_number = 0
 
         # Setting the position of the number on the player ensures that a new
         # number will be generated.
         self.number_pos = self.player.head()
-        self.current_number = 0
-        self.new_number()
+
+        self.paused = False
+        self.won = False
+        self.running = True
+
+        self.box_region = None
+
+        print(self.window_dimensions())
+        self.win.resize(*self.window_dimensions())
+
         self.draw_game()
+        self.new_number()
         self.change_pause()
+
+    def restart_game(self):
+        self.current_level = 0
+        self.player = None
+        self.next_level()
 
     def change_pause(self):
         if self.paused:
@@ -79,104 +130,16 @@ class Game:
             self.running = False
             self.draw_box('Press space to play!')
 
-    def delete_player(self):
-        for point in self.player.position:
-            if self.level.grid[point[0]][point[1]]:
-                self.win.cursor = absolute_coordinate(point)
-                self.win.putchar(' ', bgcolor=self.level.boundary_color)
-            else:
-                self.win.cursor = absolute_coordinate(point)
-                self.win.putchar(' ', bgcolor=self.level.background_color)
+    def new_number(self):
+        self.player.grow()
+        self.current_number += 1
 
-    def draw_box(self, text):
-        self.box_region = (self.level.width // 2 - len(text) // 2 - 1,
-                           self.level.height // 2 - 2,
-                           len(text) + 2,
-                           3)
-        box = pygcurse.PygcurseTextbox(self.win,
-                                       region=self.box_region,
-                                       fgcolor=self.level.text_color,
-                                       bgcolor=self.level.box_color,
-                                       text=text)
-        box.update()
-        self.win.update()
+        while self.hit_number():
+            self.number_pos = (random.randint(0, self.level.width - 1),
+                               random.randint(0, self.level.height - 1))
 
-    def draw_game(self):
-        self.draw_level()
         self.draw_number()
-        self.draw_player()
         self.draw_info()
-
-    def draw_level(self):
-        if self.box_region is None:
-            x = 0
-            y = 0
-            width = self.level.width
-            height = self.level.height
-        else:
-            x, y, width, height = self.box_region
-
-        self.win.fill(bgcolor=self.level.background_color,
-                      region=self.box_region)
-        for i in range(x, x + width):
-            for j in range(y, y + height):
-                if self.level.grid[i][j]:
-                    self.win.cursor = absolute_coordinate((i, j))
-                    self.win.putchar(' ',
-                                     x=i,
-                                     y=j + 1,
-                                     bgcolor=self.level.boundary_color)
-
-    def draw_number(self):
-        self.win.cursor = absolute_coordinate(self.number_pos)
-        self.win.putchar(str(self.current_number % 10),
-                         fgcolor=self.level.text_color,
-                         bgcolor=self.level.background_color)
-
-    def draw_player(self):
-        if self.box_region is None:
-            x = 0
-            y = 0
-            width = self.level.width
-            height = self.level.height
-        else:
-            x, y, width, height = self.box_region
-
-        # Draw the player
-        for point in self.player.position:
-            if x <= point[0] < x + width and y <= point[1] < y + height:
-                self.win.cursor = absolute_coordinate(point)
-                self.win.putchar(' ', bgcolor=self.level.player_color)
-
-    def draw_info(self):
-        width, height = self.total_dimensions()
-        self.win.fill(bgcolor=self.level.background_color,
-                      region=(0, 0, width, 1))
-        self.win.cursor = (0, 0)
-        self.win.putchars(('Score: ' + str(self.current_number - 1)
-                           + '/' + str(self.level.goal)),
-                          fgcolor=self.level.text_color,
-                          bgcolor=self.level.background_color)
-        self.win.cursor = (width - 10, 0)
-        self.win.putchars('Lives: ' + str(self.player.lives),
-                          fgcolor=self.level.text_color,
-                          bgcolor=self.level.background_color)
-
-    def death(self):
-        self.running = False
-        if self.player.lives == 1:
-            self.player.lives -= 1
-            self.draw_box('Game Over! Play again? y/n')
-        else:
-            self.reset_player()
-
-    def hit_number(self):
-        return (self.level.collision(self.number_pos) or
-                self.player.collision(self.number_pos))
-
-    def is_dead(self):
-        return (self.level.collision(self.player.head()) or
-                self.player.collision(self.player.head(), skip_head=True))
 
     def loop(self):
         event = pygame.event.poll()
@@ -210,40 +173,129 @@ class Game:
 
         if self.running:
             self.next_frame()
-        self.clock.tick_busy_loop(self.player.speed)
+        self.clock.tick_busy_loop(self.level.speed)
 
     def next_frame(self):
         old_tail = self.player.move()
-        self.level.warp_player_head(self.player)
+        self.player.warp_player_head()
         if self.is_dead():
             self.death()
             return
 
         self.win.cursor = absolute_coordinate(self.player.head())
-        self.win.putchar(' ', bgcolor=self.level.player_color)
+        self.win.putchar(' ', bgcolor=self.player_color)
 
         if old_tail is not None:
             self.win.cursor = absolute_coordinate(old_tail)
-            self.win.putchar(' ', bgcolor=self.level.background_color)
+            self.win.putchar(' ', bgcolor=self.background_color)
 
         if self.hit_number():
             if self.current_number == self.level.goal:
+                self.current_number += 1
+                self.draw_info()
                 self.running = False
-                self.won = True
-                self.draw_box('You won! Play again? y/n')
+                self.next_level()
                 return
             self.new_number()
 
-    def new_number(self):
-        self.player.grow()
-        self.current_number += 1
+    def delete_player(self):
+        for point in self.player.position:
+            if self.level.grid[point[0]][point[1]]:
+                self.win.cursor = absolute_coordinate(point)
+                self.win.putchar(' ', bgcolor=self.boundary_color)
+            else:
+                self.win.cursor = absolute_coordinate(point)
+                self.win.putchar(' ', bgcolor=self.background_color)
 
-        while self.hit_number():
-            self.number_pos = (random.randint(0, self.level.width - 1),
-                               random.randint(0, self.level.height - 1))
+    def draw_box(self, text):
+        self.box_region = (self.level.width // 2 - len(text) // 2 - 1,
+                           self.level.height // 2 - 2,
+                           len(text) + 2,
+                           3)
+        box = pygcurse.PygcurseTextbox(self.win,
+                                       region=self.box_region,
+                                       fgcolor=self.text_color,
+                                       bgcolor=self.box_color,
+                                       text=text)
+        box.update()
+        self.win.update()
 
+    def draw_game(self):
+        self.draw_level()
         self.draw_number()
+        self.draw_player()
         self.draw_info()
+
+    def draw_level(self):
+        if self.box_region is None:
+            x = 0
+            y = 0
+            width = self.level.width
+            height = self.level.height
+        else:
+            x, y, width, height = self.box_region
+
+        self.win.fill(bgcolor=self.background_color,
+                      region=self.box_region)
+        for i in range(x, x + width):
+            for j in range(y, y + height):
+                if self.level.grid[i][j]:
+                    self.win.cursor = absolute_coordinate((i, j))
+                    self.win.putchar(' ',
+                                     x=i,
+                                     y=j + 1,
+                                     bgcolor=self.boundary_color)
+
+    def draw_number(self):
+        self.win.cursor = absolute_coordinate(self.number_pos)
+        self.win.putchar(str(self.current_number % 10),
+                         fgcolor=self.text_color,
+                         bgcolor=self.background_color)
+
+    def draw_player(self):
+        if self.box_region is None:
+            x = 0
+            y = 0
+            width = self.level.width
+            height = self.level.height
+        else:
+            x, y, width, height = self.box_region
+
+        # Draw the player
+        for point in self.player.position:
+            if x <= point[0] < x + width and y <= point[1] < y + height:
+                self.win.cursor = absolute_coordinate(point)
+                self.win.putchar(' ', bgcolor=self.player_color)
+
+    def draw_info(self):
+        width, height = self.window_dimensions()
+        self.win.fill(bgcolor=self.background_color,
+                      region=(0, 0, width, 1))
+        self.win.cursor = (0, 0)
+        self.win.putchars(('Score: ' + str(self.current_number - 1)
+                           + '/' + str(self.level.goal)),
+                          fgcolor=self.text_color,
+                          bgcolor=self.background_color)
+        self.win.cursor = (width - 10, 0)
+        self.win.putchars('Lives: ' + str(self.player.lives),
+                          fgcolor=self.text_color,
+                          bgcolor=self.background_color)
+
+    def death(self):
+        self.running = False
+        if self.player.lives == 1:
+            self.player.lives -= 1
+            self.draw_box('Game Over! Play again? y/n')
+        else:
+            self.reset_player()
+
+    def hit_number(self):
+        return (self.level.collision(self.number_pos) or
+                self.player.collision(self.number_pos))
+
+    def is_dead(self):
+        return (self.level.collision(self.player.head()) or
+                self.player.collision(self.player.head(), skip_head=True))
 
     def reset_player(self):
         self.delete_player()
@@ -264,49 +316,35 @@ class Game:
         self.new_number()
         self.change_pause()
 
-    def restart_game(self):
-        self.level = Level(LEVEL)
-        self.player = Player(LEVEL)
-
-        self.running = True
-        self.won = False
-
-        self.current_number = 0
-
-        # Setting the position of the number on the player ensures that a new
-        # number will be generated.
-        self.number_pos = self.player.head()
-
-        # This will be the set to the coordinates of a box on the screen later.
-        self.box_region = None
-
-        self.win.resize(*self.total_dimensions())
-
-        self.draw_game()
-        self.new_number()
-        self.change_pause()
-
-    def total_dimensions(self):
-        return self.level.width, self.level.height + 1
+    def window_dimensions(self):
+        return self.width, self.height + 1
 
 
 class Level:
-    def __init__(self, filename):
+    def __init__(self, filename, width, height):
         # Load the configuration file
         config = configparser.ConfigParser()
         config.read(filename)
 
-        self.player_color = pygame.Color(config['Colors']['Player'])
-        self.boundary_color = pygame.Color(config['Colors']['Boundary'])
-        self.background_color = pygame.Color(config['Colors']['Background'])
-        self.text_color = pygame.Color(config['Colors']['Text'])
-        self.box_color = pygame.Color(config['Colors']['Box'])
+        self.width = width
+        self.height = height
 
-        self.width = int(config['Dimensions']['Width'])
-        self.height = int(config['Dimensions']['Height'])
+        self.goal = int(config['Level']['Goal'])
 
-        self.goal = int(config['More']['Goal'])
-        self.next_level = config['More']['Nextlevel']
+        self.initial_direction = config['Level']['Direction']
+        if self.initial_direction == 'up':
+            self.initial_direction = UP
+        elif self.initial_direction == 'down':
+            self.initial_direction = DOWN
+        elif self.initial_direction == 'left':
+            self.initial_direction = LEFT
+        elif self.initial_direction == 'right':
+            self.initial_direction = RIGHT
+
+        self.growth = int(config['Level']['Growth'])
+        self.speed = int(config['Level']['Speed'])
+        self.initial_position = (int(config['Level']['Initialx']),
+                                 int(config['Level']['Initialy']))
 
         # Create the grid where True is an obstacle and False is empty
         self.grid = []
@@ -363,39 +401,19 @@ class Level:
             else:
                 decision += slope_sign * 2 * dy
 
-    def warp_player_head(self, player):
-        """Puts the player head back into the level if it went outside."""
-        player.position[-1] = (player.position[-1][0] % self.width,
-                               player.position[-1][1] % self.height)
-
 
 class Player:
-    def __init__(self, filename):
-        # Load the configuration file
-        config = configparser.ConfigParser()
-        config.read(filename)
+    def __init__(self, level, lives):
+        self.level = level
+        self.lives = lives
+        self.direction = self.level.initial_direction
+        self.position = self.level.initial_position
 
-        self.growth = int(config['Player']['Growth'])
-        self.lives = int(config['Player']['Lives'])
-        self.speed = int(config['Player']['Speed'])
+        # How much should the player grow until it stops.
         self.to_grow = 0
 
-        self.start_direction = config['Player']['Direction']
-        if self.start_direction == 'up':
-            self.start_direction = UP
-        elif self.start_direction == 'down':
-            self.start_direction = DOWN
-        elif self.start_direction == 'left':
-            self.start_direction = LEFT
-        elif self.start_direction == 'right':
-            self.start_direction = RIGHT
-        self.direction = self.start_direction
-
-        self.startx = int(config['Player']['Startx'])
-        self.starty = int(config['Player']['Starty'])
-
         # This array will contain all coordinates occupied by the player
-        self.position = [(self.startx, self.starty)]
+        self.position = [self.level.initial_position]
 
     def collision(self, coordinate, skip_head=False):
         """Checks whether the player occupies the coordinate. If skip_head is
@@ -406,7 +424,7 @@ class Player:
             return coordinate in self.position
 
     def grow(self):
-        self.to_grow += self.growth
+        self.to_grow += self.level.growth
 
     def head(self):
         """Returns the coordinate of the head of the player."""
@@ -429,9 +447,9 @@ class Player:
             return None
 
     def next_life(self):
-        self.direction = self.start_direction
+        self.direction = self.level.initial_direction
         self.lives -= 1
-        self.position = [(self.startx, self.starty)]
+        self.position = [self.level.initial_position]
         self.to_grow = 0
 
     def tail(self):
@@ -442,9 +460,14 @@ class Player:
         """Removes the tail of the player."""
         del self.position[0]
 
+    def warp_player_head(self):
+        """Puts the player head back into the level if it went outside."""
+        self.position[-1] = (self.position[-1][0] % self.level.width,
+                             self.position[-1][1] % self.level.height)
+
 
 if __name__ == "__main__":
     pygame.init()
     game = Game(pygame.time.Clock())
-    while 1:
+    while True:
         game.loop()
